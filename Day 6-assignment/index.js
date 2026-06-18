f81e8fc5117a6e3a7f86565171d7d2d50c26781a
shivamrajjput151-spec <shivam.rajjput.151@gmail.com>
2026-06-18 18:26:19 +0530

    Create index.js

---
+++ Day 6-assignment/index.js
@@ -0,0 +1,11 @@
+// Q1) Create an empty array called fruits and add "Apple", "Mango", and "Banana" 
+// using push(). Print the array.
+
+let fruits=[];
+
+fruits.push("Apple")
+fruits.push("Mango")
+fruits.push("Banana")
+
+console.log(fruits);
+
